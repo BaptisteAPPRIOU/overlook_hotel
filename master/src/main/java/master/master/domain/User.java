5e9b8cfae4da238b8fc1d8ad5b4e41d3245ab60d
lyes-hamici <lyes.hamici@laplateforme.io>
2025-05-19 11:47:28 +0200

    feat: add user entity, other entity files are a work in progress

--- master/src/main/java/master/master/domain/User.java
+++ master/src/main/java/master/master/domain/User.java
@@ -1,4 +1,52 @@
 package master.master.domain;
 
-public class User {
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "\"user\"")
+@Access(AccessType.FIELD)
+@NamedEntityGraphs({
+        @NamedEntityGraph(
+                name = "User.role",
+                attributeNodes = @NamedAttributeNode("role")
+        )
+})
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    @SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq", allocationSize = 1)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+    @Column(nullable = false, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleType role;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return id != null && id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
 }
