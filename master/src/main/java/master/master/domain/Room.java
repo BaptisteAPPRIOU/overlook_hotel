b08a26dcbec98565562610d2187679e978c75868
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-06-09 15:02:06 +0200

    refactor: simplify Room class documentation and retain Lombok usage

--- master/src/main/java/master/master/domain/Room.java
+++ master/src/main/java/master/master/domain/Room.java
@@ -1,22 +1,11 @@
+// src/main/java/master/master/domain/Room.java
 package master.master.domain;
 
 import jakarta.persistence.*;
+import lombok.Data;       // or import lombok.Getter; import lombok.Setter;
 
-/**
- * Represents a hotel room entity.
- * <p>
- * Each room has a unique room number, a type (as defined by {@link RoomType}),
- * and an occupancy status.
- * </p>
- *
- * <ul>
- *   <li>{@code id} - The unique identifier for the room (auto-generated).</li>
- *   <li>{@code roomNumber} - The unique number assigned to the room.</li>
- *   <li>{@code roomType} - The type/category of the room.</li>
- *   <li>{@code isOccupied} - Indicates whether the room is currently occupied.</li>
- * </ul>
- */
 @Entity
+@Data                      // generates getters, setters, toString, equals & hashCode
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
