3642f7dfd4962e73635489c67c074ee006c5cfc9
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-06-09 15:02:24 +0200

    feat: add RoomRepository interface for Room entity persistence

---
+++ master/src/main/java/master/master/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package master.master.repository;
+
+import master.master.domain.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
