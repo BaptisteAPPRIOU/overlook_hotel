418c57f2519a06e945662221ebe026d85319df89
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-07-07 10:36:36 +0200

    feat : employee worday gestion feature
    Signed-off-by: thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>

---
+++ master/src/main/java/master/master/web/rest/dto/RejectLeaveRequestDto.java
@@ -0,0 +1,22 @@
+package master.master.web.rest.dto;
+
+public class RejectLeaveRequestDto {
+    private String rejectionReason;
+    private String rejectionComment;
+
+    public String getRejectionReason() {
+        return rejectionReason;
+    }
+
+    public void setRejectionReason(String rejectionReason) {
+        this.rejectionReason = rejectionReason;
+    }
+
+    public String getRejectionComment() {
+        return rejectionComment;
+    }
+
+    public void setRejectionComment(String rejectionComment) {
+        this.rejectionComment = rejectionComment;
+    }
+}
