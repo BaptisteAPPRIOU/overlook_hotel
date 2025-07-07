b94a0052928c3e02f7f614693a78f240709e1cac
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-07-07 10:07:31 +0200

    feat: add custom query to RoomRepository for retrieving rooms sorted by ID in descending order

--- master/src/main/java/master/master/repository/RoomRepository.java
+++ master/src/main/java/master/master/repository/RoomRepository.java
@@ -2,8 +2,13 @@ package master.master.repository;
 
 import master.master.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    // Custom query to find a Room and sort them by ID in descending order
+    @Query("SELECT r FROM Room r ORDER BY r.id DESC")
+    Room findAllRoom();
 }
