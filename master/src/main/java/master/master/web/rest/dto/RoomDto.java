da7f3494f9530a98b4709e9158a8f627297f0310
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-07-08 11:49:16 +0200

    refactor: reorganize RoomDto properties for better readability

--- master/src/main/java/master/master/web/rest/dto/RoomDto.java
+++ master/src/main/java/master/master/web/rest/dto/RoomDto.java
@@ -2,7 +2,7 @@ package master.master.web.rest.dto;
 
 import lombok.Builder;
 import lombok.Data;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -11,9 +11,20 @@ public class RoomDto {
     private Long id;
     private String number;
     private String type;
-    private String status; // AVAILABLE, OCCUPIED, MAINTENANCE
-    private Double price;
     private Integer capacity;
     private String description;
+    private Integer floor_number;
+    private boolean has_air_conditionning;
+    private boolean has_projector;
+    private boolean has_video_conference;
+    private boolean has_whiteboard;
+    private LocalDateTime last_maintenance_date;
+    private LocalDateTime next_maintenance_date;
+    private String name;
+    private Double price;
+    private String status; // AVAILABLE, OCCUPIED, MAINTENANCE
+    private LocalDateTime created_at;
+    private String created_by;
+    private LocalDateTime updated_at;
     private List<String> amenities;
 }
