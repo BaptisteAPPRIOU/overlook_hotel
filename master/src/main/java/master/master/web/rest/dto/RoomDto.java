b6571debdaf11db89fea40ef495c5aa479143422
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-07-08 11:51:34 +0200

    refactor: add missing Lombok annotations for getter and setter methods in RoomDto

--- master/src/main/java/master/master/web/rest/dto/RoomDto.java
+++ master/src/main/java/master/master/web/rest/dto/RoomDto.java
@@ -2,9 +2,14 @@ package master.master.web.rest.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
 @Data
 @Builder
 public class RoomDto {
