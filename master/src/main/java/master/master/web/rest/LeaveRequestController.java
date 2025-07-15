190bacda9606f25db24747796bc29fd06de2b558
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-07-15 15:03:21 +0200

    refactor: reorganize import statements in LeaveRequestController for improved clarity

--- master/src/main/java/master/master/web/rest/LeaveRequestController.java
+++ master/src/main/java/master/master/web/rest/LeaveRequestController.java
@@ -1,15 +1,25 @@
 package master.master.web.rest;
 
-import master.master.service.LeaveRequestService;
-import master.master.web.rest.dto.CreateLeaveRequestDto;
-import master.master.web.rest.dto.LeaveRequestDto;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import master.master.service.LeaveRequestService;
+import master.master.web.rest.dto.CreateLeaveRequestDto;
+import master.master.web.rest.dto.LeaveRequestDto;
 
 /**
  * REST controller for managing leave requests.
